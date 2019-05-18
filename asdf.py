bd689151573a77c71428bc5907f0ee8cc735a00f
hdhdudj <dnjswbf@hanmail.net>
2019-05-18 13:23:12 +0900

    first commit

---
+++ asdf.py
@@ -0,0 +1,4 @@
+names = ['A', 'B', 'C', 'D']
+
+for i in range(1,10, 2):
+    print(i)
---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default= timezone.now
+    )
+    publised_date = models.DateTimeField(
+        blank=True, null = False
+    )
+
+    def publish(self):
+        self.publised_date = timezone.now()
+        self.save()
+    def __str__(self):
+        return self.title
